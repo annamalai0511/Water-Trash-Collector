5f73194044fa5a8b4349e488d8df4cfad4dd38a2
Sure Annamalai <73933212+annamalai0511@users.noreply.github.com>
2023-05-08 01:58:02 +0530

    Add files via upload

---
+++ S_test/car.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as gpio
+import time
+
+
+def cars(cmdd):
+    if cmdd == "straight":
+        forward(2)
+    elif cmdd == "left" :
+        turn_left(2)
+    elif cmdd == "right" : 
+        turn_right(2)
+    else:
+        return 0
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+def forward(tf):
+    init()
+    gpio.output(13, True)
+    gpio.output(15, False)
+    gpio.output(7, False)
+    gpio.output(11, True)
+    time.sleep(tf)
+    gpio.cleanup()
+    
+def reverse(tf):
+    init()
+    gpio.output(13, False)
+    gpio.output(15, True)
+    gpio.output(7, True)
+    gpio.output(11, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def turn_left(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def turn_right(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(13, False)
+    gpio.output(15, False)
+    gpio.output(11, True)
+    time.sleep(tf)
+    gpio.cleanup()
